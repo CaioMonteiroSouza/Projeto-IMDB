f00d4f5436aca9937c8c85f062ea83b4c353849d
Caio <caioeduardo1403@gmail.com>
2024-06-27 15:47:32 -0300

    Script do desafio completo

--- Sprint 7/Desafio/Script.py
+++ Sprint 7/Desafio/Script.py
@@ -2,27 +2,13 @@ import requests
 from dotenv import load_dotenv
 import os
 import pandas as pd
+import json
+from datetime import datetime
 
-df = pd.read_csv('Sprint 6\\Desafio\\data\\series.csv', sep='|', low_memory=False)
-
-genero = 'Animation'
-ids_filtrados = df[df['genero'] == genero]['id']
-
-load_dotenv()
-
-API_KEY = os.getenv("API_KEY")
-TOKEN_LEITURA = os.getenv("TOKEN_LEITURA")
-
-
-headers = {
-    "accept": "application/json",
-    "Authorization": f'Bearer {TOKEN_LEITURA}'
-}
 
 def get_movie_internal_id(data):
     if 'tv_results' in data:
         if not data['tv_results']: 
-            print("tv_results está vazio.")
             return None
         for movie in data['tv_results']:
             filme_id = movie.get('id', 'id não encontrado')
@@ -44,22 +30,105 @@ def send_serie_internal_id_request(serie_id):
     return requests.get(f'https://api.themoviedb.org/3/tv/{serie_id}?language=en-US', headers=headers)
 
 
-def get_serie_details(serie_id):
+def get_serie_details(serie_id, file_index):
     response = send_serie_external_id_request(serie_id)
     if response:
         data = turn_to_json(response)
         internal_id = get_movie_internal_id(data)
         response_details = send_serie_internal_id_request(internal_id)
         data_details = turn_to_json(response_details)
-        print(data_details)
-    else: 
-        print('filme não encontrado')
-        return
+        if data_details.get("success", True):
+            filename = f'Sprint 7\\Desafio\\Jsons\\Complementar_Csv\\series_{file_index}.json'
+            with open(filename, 'a') as f:
+                json.dump(data_details, f)
+                f.write('\n')
+                return True
+        else:
+            return False
+
 
 def send_requisitions(ids_filtrados):
-    ids_filtrados = ids_filtrados.tolist()
+    file_index = proximo_numero_serie('Sprint 7\\Desafio\\Jsons\\Complementar_Csv')
+    count = 0
     for serie_id in ids_filtrados:
-        get_serie_details(serie_id)
+        if (get_serie_details(serie_id, file_index)):
+            count += 1
+            if count >= 99:
+                file_index += 1
+                count = 0
 
 
-send_requisitions(ids_filtrados)
+def proximo_numero_serie(diretorio):
+    numbers = []
+    if len(os.listdir(diretorio)):
+        for name in os.listdir(diretorio):
+            if name.startswith('series_') and name.endswith('.json'):
+                    number = int(name[7:-5])
+                    numbers.append(number)
+    else: return 1
+    return max(numbers) + 1
+
+
+def get_top_rated_series():
+    response = requests.get('https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1', headers=headers)
+    if response:
+        data = turn_to_json(response)
+        filtered_results = [item for item in data["results"] if 16 in item["genre_ids"]]
+        
+        filename = f'Sprint 7\\Desafio\\Jsons\\Top_Rated\\{year}\\{month}\\{day}\\top_rated_{year}-{month}-{day}.json'
+        with open(filename, 'a') as f:
+            for item in filtered_results:
+                json.dump(item, f)
+                f.write('\n')
+        return True
+
+
+def get_popular_series():
+    response = requests.get('https://api.themoviedb.org/3/tv/popular?language=en-US&page=1', headers=headers)
+    if response:
+        data = turn_to_json(response)
+        filtered_results = [item for item in data["results"] if 16 in item["genre_ids"]]
+        
+        filename = f'Sprint 7\\Desafio\\Jsons\\Popular\\{year}\\{month}\\{day}\\Popular_{year}-{month}-{day}.json'
+        with open(filename, 'a') as f:
+            for item in filtered_results:
+                json.dump(item, f)
+                f.write('\n')
+        return True
+
+if __name__ == "__main__":
+    now = datetime.now()
+    day = now.day
+    month = now.month
+    year = now.year
+
+    df = pd.read_csv('Sprint 6\\Desafio\\data\\series.csv', sep='|', low_memory=False)
+
+    directory_path_top_rated = f'Sprint 7/Desafio/Jsons/Top_Rated/{year}/{month}/{day}/'
+    os.makedirs(directory_path_top_rated, exist_ok=True)
+
+    directory_path_popular = f'Sprint 7/Desafio/Jsons/Popular/{year}/{month}/{day}/'
+    os.makedirs(directory_path_popular, exist_ok=True)
+
+    genero = 'Animation'
+    ids_filtrados = df[df['genero'] == genero]['id']
+    ids_filtrados = list(set(ids_filtrados))
+
+
+    load_dotenv()
+
+
+    API_KEY = os.getenv("API_KEY")
+    TOKEN_LEITURA = os.getenv("TOKEN_LEITURA")
+
+    headers = {
+    "accept": "application/json",
+    "Authorization": f'Bearer {TOKEN_LEITURA}'
+    }
+
+    
+    send_requisitions(ids_filtrados)
+
+    get_top_rated_series()
+
+    get_popular_series()
